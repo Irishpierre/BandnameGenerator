ca785d7db75cb80605866ffaf394b89daebd3ec3
Irishpierre <65203153+Irishpierre@users.noreply.github.com>
2020-12-28 13:00:29 -0500

    Add files via upload
    Main file for the band name generator

---
+++ MainFile.py
@@ -0,0 +1,14 @@
+#Create a greeting
+print("This is the Band Name Generator!")
+
+#Ask the user for their city
+city = input("Which city did you grow up in?\n")
+
+#Ask for the name of a pet
+pet = input("What is the name of a pet?\n")
+
+#Combine their city and pet
+print("Your rockin' band name is " + city + " " + pet)
+
+#Make sure the cursor shows on a new line
+#Dont forget \n for a new line
